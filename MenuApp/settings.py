e85e8fee38e877ba286c9cca6e18a9d45de694c8
Régis Melgaço de Andrade <regis.melgaco@gmail.com>
2019-05-16 19:24:05 -0300

    database setup

--- MenuApp/settings.py
+++ MenuApp/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'MenuApp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'menuapp',
+        'USER': 'menu_app_user',
+        'PASSWORD': 'bananatastico3516',
+        'HOST': '127.0.0.1',
+        'PORT': '5432'
     }
 }
 
@@ -103,7 +107,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
 TIME_ZONE = 'UTC'
 
