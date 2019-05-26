e3cb19c66e9f97ed4bc2de95efa9a52e48f95525
Régis Melgaço de Andrade <regis.melgaco@gmail.com>
2019-05-26 09:32:37 -0300

    fix plate api path

--- Order/urls.py
+++ Order/urls.py
@@ -6,7 +6,7 @@ app_name = 'Order'
 
 
 router = routers.DefaultRouter()
-router.register('api/order', views.PlateViewSet, base_name='order')
+router.register('api/plate', views.PlateViewSet, base_name='order')
 router.register('api/menu', views.MenuViewSet, base_name='menu')
 
 urlpatterns = router.urls
